04963e4e24529c63156d7f777cf2def258adb452
qlj <qlj2009@gmail.com>
2011-11-16 21:10:00 +0800

    Initial Commit

---
+++ CustomNavigationBarController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomNavigationBarController
+//
+//  Created by LiJian Qiu on 11-11-16.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
